5045dcb26404d4341077343d131459d26e68b40d
okami89 <fractalbeach@gmail.com>
2012-12-23 13:10:03 -0500

    Added scripts, including one to calculate local-server time offset. Made a homepage that uses scripts.

---
+++ script/sync.js
@@ -0,0 +1,30 @@
+/**
+ * sync.js
+ *
+ * Synchronize with the offset between the client's time and the server's time.
+ */
+
+var maestro = {};
+$( document ).ready(
+    function() {
+	maestro.utils = {};
+	maestro.utils.getTimeOffset = function() {
+	    // Current client time
+	    var localMillisUTC = Date.parse( new Date().toUTCString() );
+
+	    // AJAX request to find server date
+	    var request = new XMLHttpRequest();
+	    request.open( 'HEAD', document.location, false );
+	    request.send( null );
+	    var serverDate = request.getResponseHeader( 'date' );
+	    var serverTimeMillisUTC = Date.parse( new Date(Date.parse(serverDate)).toUTCString() );
+
+	    // The offset
+	    var offset = serverTimeMillisUTC - localMillisUTC;
+	    return offset;
+	};
+
+	var offset = maestro.utils.getTimeOffset();
+	alert("Your offset from server time is "+offset);
+    }
+);
