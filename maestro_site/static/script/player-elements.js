36e94a23c2642f3d237572466c0f745dc9736ea5
Luke Lovett <llovett@oberlin.edu>
2013-01-21 20:01:39 -0500

    song stem instruments are inserted into the instrument select div

--- maestro_site/static/script/player-elements.js
+++ maestro_site/static/script/player-elements.js
@@ -1,11 +1,37 @@
 $(document).ready(
     function() {
+	$("#controls").hide();
+	
 	$("#song_select_list").click(
 	    function() {
 		var songname = encodeURIComponent($("#song_select_list option:selected").text());
 		$.get( '/stemget/?title='+songname,
 		       function( data ) {
-			   alert(data);
+			   console.log(data);
+
+			   // Show controls
+			   $("#controls").show();
+
+			   // De-populate instrument select list
+			   $("#instrument_select").empty();
+
+			   // Populate instrument select list
+			   var stems = data.stems;
+			   for ( var i=0; i<stems.length; i++ ) {
+			       var newitem = $("<li></li>");
+			       newitem.addClass( "instrument_"+i );
+			       var newlink = $("<a></a>");
+			       newlink.attr( {href:"#" } );
+			       newlink.append( stems[i].instr );
+			       newitem.click(
+				   function( event ) {
+				       event.preventDefault();
+				       $(this).toggleClass( "selected" );
+				   }
+			       );
+			       newitem.append( newlink );
+			       $("#instrument_select").append( newitem );
+			   }
 		       } );
 	    }
 	);
